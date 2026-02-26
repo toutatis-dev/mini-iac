957f128980ba7c1b8d8e7b74be324f30628732f6
toutatis-dev <jack.benmiller1@protonmail.com>
2026-02-26 10:03:33 -0800

    feat: complete tokenising logic

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -48,9 +48,27 @@ func (l *Lexer) NextToken() token.Token {
 	case ';':
 		tok.Type = token.SEMICOLON
 		tok.Literal = ";"
-	case '0':
+	case '=':
+		tok.Type = token.ASSIGN
+		tok.Literal = "="
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
+	case 0:
 		tok.Type = token.EOF
 		tok.Literal = "EOF"
+	default:
+		if isLetter(l.Char) {
+			ident := l.readIdentifier()
+
+			tok.Type = token.LookupIdent(ident)
+			tok.Literal = ident
+			return tok
+		} else {
+			tok.Type = token.ILLEGAL
+			tok.Literal = string(l.Char)
+		}
+
 	}
 
 	l.readChar()
@@ -61,4 +79,34 @@ func (l *Lexer) skipWhiteSpace() {
 	for l.Char == ' ' || l.Char == '\t' || l.Char == '\n' || l.Char == '\r' {
 		l.readChar()
 	}
+}
+
+func (l *Lexer) readIdentifier() string {
+
+	initPos := l.CurrentChar
+
+	for isLetter(l.Char) {
+		l.readChar()
+	}
+
+	return l.Input[initPos:l.CurrentChar]
+
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func (l *Lexer) readString() string {
+	firstLetter := l.NextChar
+
+	for {
+		l.readChar()
+		if l.Char == '"' || l.Char == 0 {
+			break
+		}
+
+	}
+	return l.Input[firstLetter:l.CurrentChar]
+
 }
