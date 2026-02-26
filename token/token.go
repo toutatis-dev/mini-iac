d5c9e4793a3a5d6b3224d760be1f39749d8b275e
toutatis-dev <jack.benmiller1@protonmail.com>
2026-02-26 10:03:07 -0800

    feat: implement LookupIdent

--- token/token.go
+++ token/token.go
@@ -14,7 +14,19 @@ const (
 	ILLEGAL   TokenType = "Illegal"
 )
 
+var Keywords = map[string]TokenType{
+	"resource": KEYWORD,
+}
+
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := Keywords[ident]; ok {
+		return tok
+	} else {
+		return IDENT
+	}
 }
