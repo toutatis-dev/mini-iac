52161165d2f279cb49b1d40eb1f2477b29adf5d8
toutatis-dev <jack.benmiller1@protonmail.com>
2026-02-26 11:43:31 -0800

    feat: implement parseResource

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -41,11 +41,73 @@ func (p *Parser) ParseManifest() ast.Manifest {
 			switch p.curToken.Literal {
 			case "resource":
 				res := p.parseResource()
-				manifest.Blocks = append(manifest.Blocks, res)
+				if res != nil {
+					manifest.Blocks = append(manifest.Blocks, res)
+				}
+
+			default:
+
 			}
 		}
 		p.nextToken()
 	}
 
 	return manifest
+}
+
+func (p *Parser) parseResource() ast.Block {
+	//resource definition must follow pattern resource string string {
+
+	res := &ast.Resource{
+		Properties: make(map[string]string),
+	}
+	prov := p.expectPeek(token.STRING)
+	if prov {
+		res.Provider = p.curToken.Literal
+	} else {
+		return nil
+	}
+
+	name := p.expectPeek(token.STRING)
+	if name {
+		res.ResourceName = p.curToken.Literal
+	} else {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	for p.peekToken.Type != token.RBRACE && p.peekToken.Type != token.EOF {
+		//properties are ident, assign, string, semicolon
+		p.nextToken()
+		key := p.curToken.Literal //save property type as key for map
+		if !p.expectPeek(token.ASSIGN) {
+			return nil
+		}
+		if !p.expectPeek(token.STRING) {
+			return nil
+		}
+		res.Properties[key] = p.curToken.Literal //load property value into map
+		if !p.expectPeek(token.SEMICOLON) {
+			return nil
+		}
+	}
+	if !p.expectPeek(token.RBRACE) {
+		return nil
+	}
+	return res
+
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+
+	if t == p.peekToken.Type {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+
 }
