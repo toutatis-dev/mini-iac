7f615ffb2e64178876d041dd7b3b0985e7046ae5
toutatis-dev <jack.benmiller1@protonmail.com>
2026-02-26 10:28:47 -0800

    feat: implement constructor for parser

---
+++ internal/parser/parser.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"mini-iac/internal/lexer"
+	"mini-iac/internal/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
