1279ea18db4ad899dc918ae4960271feb19c23ac
toutatis-dev <jack.benmiller1@protonmail.com>
2026-03-04 12:32:22 -0800

    feat: implement entry points for program up to plan

--- main.go
+++ main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"mini-iac/internal/lexer"
 	"mini-iac/internal/parser"
+	"mini-iac/internal/planner"
+	"mini-iac/internal/providers/file"
+	"mini-iac/internal/state"
+	"os"
 )
 
 func main() {
-	input := `
-	resource "file" "main.go" {
-		content = "package main";
-	}
 
-	resource "file" "readme.md" {
-		content = "# My Project";
+	filePath := os.Args[1:]
+
+	bcontents, err := os.ReadFile(filePath[0])
+	if err != nil {
+		log.Fatal(err)
 	}
-	`
+	input := string(bcontents)
 	lex := lexer.New(input)
 	p := parser.New(lex)
 
 	manifest := p.ParseManifest()
+	s := state.NewState()
+	s.LoadState()
+
+	plan, err := planner.Planner(&s, &manifest, &file.FileProvider{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Printf("Parsed %d resources:\n", len(manifest.Blocks))
 	for i, block := range manifest.Blocks {
