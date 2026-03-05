6b23b0dfbfcd437985926485a3a620677ac8be23
toutatis-dev <jack.benmiller1@protonmail.com>
2026-03-05 12:15:22 -0800

    feat: implement entry to executor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"mini-iac/internal/executor"
 	"mini-iac/internal/lexer"
 	"mini-iac/internal/parser"
 	"mini-iac/internal/planner"
@@ -27,10 +28,18 @@ func main() {
 	s := state.NewState()
 	s.LoadState()
 
+	//pass manifest and state to planner. Hard code provider for now
 	plan, err := planner.Planner(&s, &manifest, &file.FileProvider{})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//pass plan to executor
+	err = executor.Execute(plan, &file.FileProvider{}, &s)
+	if err != nil {
+		log.Fatalf("Could not execute plan")
+	}
+	s.SaveState()
 
 	fmt.Printf("Parsed %d resources:\n", len(manifest.Blocks))
 	for i, block := range manifest.Blocks {
