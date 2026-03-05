4bced98550e8b1ae237e4c3b069a9b1d1d6ec6e2
toutatis-dev <jack.benmiller1@protonmail.com>
2026-03-05 12:15:41 -0800

    feat: implement Execute

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -1,10 +1,43 @@
 package executor
 
 import (
+	"errors"
+	"log"
 	"mini-iac/internal/planner"
 	"mini-iac/internal/provider"
 	"mini-iac/internal/state"
 )
 
 func Execute(plan planner.Plan, prov provider.ResourceProvider, s *state.State) error {
+
+	for _, plans := range plan.Items {
+		switch plans.Action {
+		case planner.CREATE:
+			newState, err := prov.Create(plans.Resource)
+			if err != nil {
+				return errors.New(err.Error())
+			}
+
+			id := provider.ResourceID(plans.Resource)
+			s.State[id] = *newState
+		case planner.DELETE:
+			err := prov.Delete(plans.ID)
+			if err != nil {
+				return errors.New(err.Error())
+			}
+			delete(s.State, plans.ID)
+		case planner.NOOP:
+			continue
+		case planner.UPDATE:
+			newState, err := prov.Update(plans.Resource)
+			if err != nil {
+				return errors.New(err.Error())
+			}
+			id := provider.ResourceID(plans.Resource)
+			s.State[id] = *newState
+		default:
+			log.Println("Action not recognised")
+		}
+	}
+	return nil
 }
